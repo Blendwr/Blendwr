78a08faed5405398129f6eded4b213699f7f66e7
Blendwr <nopk.sipk@gmail.com>
2022-08-18 18:34:22 -0400

    Removed icons

--- README.md
+++ README.md
@@ -15,23 +15,23 @@ Freelance fullstack software developer
 <details>
 <summary>Programming languages</summary>
 
-- Javascript <img alt="Javascript" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png" width="20">
-- Typescript <img alt="Typescript" src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg" width="20">
-- Python <img alt="Python" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2048px-Python-logo-notext.svg.png" width="20">
-- Dart <img alt="Dart" src="https://avatars.githubusercontent.com/u/1609975?s=280&v=4" width="20">
+- Javascript
+- Typescript
+- Python
+- Dart
 </details>
 
 <details>
 <summary>Frameworks and tools</summary>
 
-- ReactJS <img alt="React" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" width="20">
-- RedwoodJS <img alt="Redwood" src="https://redwoodjs.cn/images/opengraph-256.png" width="20">
-- NodeJS <img alt="Node" src="https://nodejs.org/static/images/logo.svg" width="20">
-- ExpressJS <img alt="express" src="https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png" width="20">
-- Graphql Yoga <img alt="yoga" src="https://img.stackshare.io/service/9556/68747470733a2f2f696d6775722e636f6d2f5376366a3042362e706e67.png" width="20">
-- Flutter <img alt="flutter" src="https://io.google/2022/app/images/product-announcement-flutter.svg" width="20">
-- React Native <img alt="React native" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" width="20">
-- Expo (React Native) <img alt="Expo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png" width="20">
+- ReactJS
+- RedwoodJS
+- NodeJS
+- ExpressJS
+- Graphql Yoga
+- Flutter
+- React Native
+- Expo (React Native)
 - Flask <img alt="" src="" width="20">
 - Flask Restful <img alt="" src="" width="20">
 - Ariadne Graphql <img alt="" src="" width="20">
--- web/src/layouts/HomeLayout/HomeLayout.js
+++ web/src/layouts/HomeLayout/HomeLayout.js
@@ -15,62 +15,62 @@ import {
   Container,
 } from '@mui/material'
 import { Parallax } from 'react-parallax'
-import useScrolling from 'src/hooks/UseScrolling'
+// import useScrolling from 'src/hooks/UseScrolling'
 
 import background from './background.gif'
 
-const useMousePos = () => {
-  const [xPos, setXPos] = useState(0)
-  const [yPos, setYPos] = useState(0)
-  const [directionX, setDirectionX] = useState('left')
-  const [directionY, setDirectionY] = useState('top')
-  const [wanderingEyeDirection, setWanderingEyeDirection] = useState('0deg')
+// const useMousePos = () => {
+//   const [xPos, setXPos] = useState(0)
+//   const [yPos, setYPos] = useState(0)
+//   const [directionX, setDirectionX] = useState('left')
+//   const [directionY, setDirectionY] = useState('top')
+//   const [wanderingEyeDirection, setWanderingEyeDirection] = useState('0deg')
 
-  useEffect(() => {
-    const changeWanderingEyeDirection = () => {
-      if (directionX == 'left' && directionY == 'top') {
-        setWanderingEyeDirection('45deg')
-      } else if (directionX == 'right' && directionY == 'top') {
-        setWanderingEyeDirection('135deg')
-      } else if (directionX == 'left' && directionY == 'bottom') {
-        setWanderingEyeDirection('-45deg')
-      } else if (directionX == 'right' && directionY == 'bottom') {
-        setWanderingEyeDirection('-135deg')
-      }
-    }
+//   useEffect(() => {
+//     const changeWanderingEyeDirection = () => {
+//       if (directionX == 'left' && directionY == 'top') {
+//         setWanderingEyeDirection('45deg')
+//       } else if (directionX == 'right' && directionY == 'top') {
+//         setWanderingEyeDirection('135deg')
+//       } else if (directionX == 'left' && directionY == 'bottom') {
+//         setWanderingEyeDirection('-45deg')
+//       } else if (directionX == 'right' && directionY == 'bottom') {
+//         setWanderingEyeDirection('-135deg')
+//       }
+//     }
 
-    const handleMove = (evt) => {
-      const [oldX, oldY] = [xPos, yPos]
-      const { clientX, clientY } = evt
+//     const handleMove = (evt) => {
+//       const [oldX, oldY] = [xPos, yPos]
+//       const { clientX, clientY } = evt
 
-      if (oldX > clientX) {
-        setDirectionX('left')
-      } else {
-        setDirectionX('right')
-      }
+//       if (oldX > clientX) {
+//         setDirectionX('left')
+//       } else {
+//         setDirectionX('right')
+//       }
 
-      if (oldY > clientY) {
-        setDirectionY('top')
-      } else {
-        setDirectionY('bottom')
-      }
+//       if (oldY > clientY) {
+//         setDirectionY('top')
+//       } else {
+//         setDirectionY('bottom')
+//       }
 
-      setXPos(clientX)
-      setYPos(clientY)
-      changeWanderingEyeDirection()
-    }
+//       setXPos(clientX)
+//       setYPos(clientY)
+//       changeWanderingEyeDirection()
+//     }
 
-    window.addEventListener('mousemove', handleMove)
+//     window.addEventListener('mousemove', handleMove)
 
-    return () => window.removeEventListener('mousemove', handleMove)
-  }, [xPos, yPos, directionX, directionY])
+//     return () => window.removeEventListener('mousemove', handleMove)
+//   }, [xPos, yPos, directionX, directionY])
 
-  return { xPos, yPos, directionX, directionY, wanderingEyeDirection }
-}
+//   return { xPos, yPos, directionX, directionY, wanderingEyeDirection }
+// }
 
 const HomeLayout = ({ children }) => {
-  const scrolling = useScrolling()
-  const mousePos = useMousePos()
+  // const scrolling = useScrolling()
+  // const mousePos = useMousePos()
 
   return (
     <div>
@@ -80,7 +80,7 @@ const HomeLayout = ({ children }) => {
         bgImageAlt="background"
         strength={200}
       >
-        <img
+        {/* <img
           src="https://terraria.wiki.gg/es/images/5/59/Wandering_Eye.gif"
           style={{
             transition: 'all ease 1s',
@@ -91,7 +91,7 @@ const HomeLayout = ({ children }) => {
             zIndex: 'inherit',
           }}
           alt="ojo"
-        />
+        /> */}
         <Box>
           <AppBar
             elevation={0}
@@ -100,12 +100,12 @@ const HomeLayout = ({ children }) => {
             sx={{ backdropFilter: 'blur(20px)' }}
           >
             <Toolbar>
-              <Slide direction="down" in={scrolling} unmountOnExit>
+              {/* <Slide direction="down" in={scrolling} unmountOnExit> */}
                 <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
                   ceeeeeesar
                 </Typography>
-              </Slide>
-              <Fade in={scrolling} unmountOnExit>
+              {/* </Slide> */}
+              {/* <Fade in={scrolling} unmountOnExit> */}
                 <Button
                   color="secondary"
                   component="a"
@@ -115,7 +115,7 @@ const HomeLayout = ({ children }) => {
                 >
                   GitHub
                 </Button>
-              </Fade>
+              {/* </Fade> */}
             </Toolbar>
           </AppBar>
           <Container>{children}</Container>
