36e25a8f3cbf366b7205b394ffd451aef57461a2
Blendwr <nopk.sipk@gmail.com>
2022-08-18 19:32:55 -0400

    Fixed low performance animations

--- web/src/layouts/HomeLayout/HomeLayout.js
+++ web/src/layouts/HomeLayout/HomeLayout.js
@@ -1,12 +1,12 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Code from 'pixelarticons/svg/code.svg'
 import Contact from 'pixelarticons/svg/contact.svg'
 import {
   AppBar,
   Button,
-  // Fade,
-  // Slide,
+  Fade,
+  Slide,
   Toolbar,
   Typography,
   Box,
@@ -14,73 +14,66 @@ import {
   Fab,
   Container,
 } from '@mui/material'
-// import { Parallax } from 'react-parallax'
-// import useScrolling from 'src/hooks/UseScrolling'
+import useScrolling from 'src/hooks/UseScrolling'
 
 // import background from './background.gif'
 
-// const useMousePos = () => {
-//   const [xPos, setXPos] = useState(0)
-//   const [yPos, setYPos] = useState(0)
-//   const [directionX, setDirectionX] = useState('left')
-//   const [directionY, setDirectionY] = useState('top')
-//   const [wanderingEyeDirection, setWanderingEyeDirection] = useState('0deg')
+const useMousePos = () => {
+  const [xPos, setXPos] = useState(0)
+  const [yPos, setYPos] = useState(0)
+  const [directionX, setDirectionX] = useState('left')
+  const [directionY, setDirectionY] = useState('top')
+  const [wanderingEyeDirection, setWanderingEyeDirection] = useState('0deg')
 
-//   useEffect(() => {
-//     const changeWanderingEyeDirection = () => {
-//       if (directionX == 'left' && directionY == 'top') {
-//         setWanderingEyeDirection('45deg')
-//       } else if (directionX == 'right' && directionY == 'top') {
-//         setWanderingEyeDirection('135deg')
-//       } else if (directionX == 'left' && directionY == 'bottom') {
-//         setWanderingEyeDirection('-45deg')
-//       } else if (directionX == 'right' && directionY == 'bottom') {
-//         setWanderingEyeDirection('-135deg')
-//       }
-//     }
+  useEffect(() => {
+    const changeWanderingEyeDirection = () => {
+      if (directionX == 'left' && directionY == 'top') {
+        setWanderingEyeDirection('45deg')
+      } else if (directionX == 'right' && directionY == 'top') {
+        setWanderingEyeDirection('135deg')
+      } else if (directionX == 'left' && directionY == 'bottom') {
+        setWanderingEyeDirection('-45deg')
+      } else if (directionX == 'right' && directionY == 'bottom') {
+        setWanderingEyeDirection('-135deg')
+      }
+    }
 
-//     const handleMove = (evt) => {
-//       const [oldX, oldY] = [xPos, yPos]
-//       const { clientX, clientY } = evt
+    const handleMove = (evt) => {
+      const [oldX, oldY] = [xPos, yPos]
+      const { clientX, clientY } = evt
 
-//       if (oldX > clientX) {
-//         setDirectionX('left')
-//       } else {
-//         setDirectionX('right')
-//       }
+      if (oldX > clientX) {
+        setDirectionX('left')
+      } else {
+        setDirectionX('right')
+      }
 
-//       if (oldY > clientY) {
-//         setDirectionY('top')
-//       } else {
-//         setDirectionY('bottom')
-//       }
+      if (oldY > clientY) {
+        setDirectionY('top')
+      } else {
+        setDirectionY('bottom')
+      }
 
-//       setXPos(clientX)
-//       setYPos(clientY)
-//       changeWanderingEyeDirection()
-//     }
+      setXPos(clientX)
+      setYPos(clientY)
+      changeWanderingEyeDirection()
+    }
 
-//     window.addEventListener('mousemove', handleMove)
+    window.addEventListener('mousemove', handleMove)
 
-//     return () => window.removeEventListener('mousemove', handleMove)
-//   }, [xPos, yPos, directionX, directionY])
+    return () => window.removeEventListener('mousemove', handleMove)
+  }, [xPos, yPos, directionX, directionY])
 
-//   return { xPos, yPos, directionX, directionY, wanderingEyeDirection }
-// }
+  return { xPos, yPos, directionX, directionY, wanderingEyeDirection }
+}
 
 const HomeLayout = ({ children }) => {
-  // const scrolling = useScrolling()
-  // const mousePos = useMousePos()
+  const scrolling = useScrolling()
+  const mousePos = useMousePos()
 
   return (
     <div>
-      {/* <Parallax
-        bgImage={background}
-        blur={10}
-        bgImageAlt="background"
-        strength={200}
-      > */}
-        {/* <img
+        <img
           src="https://terraria.wiki.gg/es/images/5/59/Wandering_Eye.gif"
           style={{
             transition: 'all ease 1s',
@@ -91,7 +84,7 @@ const HomeLayout = ({ children }) => {
             zIndex: 'inherit',
           }}
           alt="ojo"
-        /> */}
+        />
         <Box>
           <AppBar
             elevation={0}
@@ -100,12 +93,12 @@ const HomeLayout = ({ children }) => {
             sx={{ backdropFilter: 'blur(20px)' }}
           >
             <Toolbar>
-              {/* <Slide direction="down" in={scrolling} unmountOnExit> */}
+              <Slide direction="down" in={scrolling} unmountOnExit>
                 <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
                   ceeeeeesar
                 </Typography>
-              {/* </Slide> */}
-              {/* <Fade in={scrolling} unmountOnExit> */}
+              </Slide>
+              <Fade in={scrolling} unmountOnExit>
                 <Button
                   color="secondary"
                   component="a"
@@ -115,7 +108,7 @@ const HomeLayout = ({ children }) => {
                 >
                   GitHub
                 </Button>
-              {/* </Fade> */}
+              </Fade>
             </Toolbar>
           </AppBar>
           <Container>{children}</Container>
@@ -128,7 +121,6 @@ const HomeLayout = ({ children }) => {
             Contact
           </Fab>
         </Box>
-      {/* </Parallax> */}
     </div>
   )
 }
